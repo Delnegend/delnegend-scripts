97db703fe86049983d19e091b50ab6cfd553e8d0
Delnegend <kiennguyen19323@gmail.com>
2022-04-02 04:58:56 +0700

    refactor(batch-jxl): impl ThreadPoolExecutor

--- BatchJXL.py
+++ BatchJXL.py
@@ -1,33 +1,15 @@
 import subprocess, os
-from multiprocessing import Process
-import time
+import concurrent.futures
 
 def main():
     files = getFiles(".\\")
-    procs = []
-
-    while True:
-
-        for proc in procs:
-            if not proc.is_alive():
-                procs.remove(proc)
-
-        if (len(procs) < 4 and len(files) > 0):
-            proc = Process(target=convert, args=(files[0],))
-            procs.append(proc)
-            proc.start()
-            files.pop(0)
-            sign(f'Queue: {str(len(procs))}, files remaining: {str(len(files))}')
-        elif (len(procs) == 0 and len(files) == 0):
-            print("the work is done")
-            break
-        time.sleep(1)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        executor.map(convert, files)
 
 def convert(file):
     filename = file.replace("."+file.split('.')[-1], "")
     subprocess.run(f'cjxl "{file}" "{filename}.jxl" -q 100 -e 8', shell=True, check=True)
 
-# get files in folder
 def getFiles(path):
     files = []
     for file in os.listdir(path):
@@ -35,13 +17,6 @@ def getFiles(path):
         if file.split('.')[-1] in supported_format:
             files.append(file)
     return files
-
-def sign(text:str):
-    print("="*40)
-    print("=\n"*2+"=")
-    print("="+"      "+text)
-    print("=\n"*2+"=")
-    print("="*40)
 
 if __name__ == '__main__':
     try:
