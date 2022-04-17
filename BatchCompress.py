7b9b95b2b8ec7cdc2932d3804ee30f3f2ca9994f
Delnegend <kiennguyen19323@gmail.com>
2022-04-18 02:42:21 +0700

    refactor!(batch-jxl)

--- BatchCompress.py
+++ BatchCompress.py
@@ -8,20 +8,27 @@ import concurrent.futures
 import subprocess
 
 def main():
-    params = params()
-    for item in [f for f in listdir(".") if not isfile(join(".", f))]:
-        chdir(item)
-        with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
-            executor.map(compress, item, repeat(params.type))
-        move(f'{item}.{type}', "..")
+    params = getParams()
+    folders = getFiles(".")
+    for folder in folders:
+        chdir(folder)
+        if params.type == "zip":
+            compress_zip(folder)
+        else if params.type == "7z":
+            compress_7z(folder)
+        move(f'{folder}.{type}', "..")
         chdir("..")
 
-def compress(item, type):
-    zip = f'7z.exe a -bt -x"!*.ini" -r "{item}.zip" *.*'
-    seven_z = f'7z a -t7z -x"!*.ini" -m0=lzma2:d1024m -mx=9 -mfb=64 -md=32m -ms=on -r "{item}.7z" *.*'
-    subprocess.run(zip if type == "zip" else seven_z, shell=True, check=True)
+def getFiles(path):
+    return [f for f in listdir(path) if not isfile(join(path, f))]
 
-def params():
+def compress_zip(item):
+    subprocess.run(zip = f'7z.exe a -bt -x"!*.ini" -r "{item}.zip" *.*', shell=True, check=True)
+
+def compress_7z(item):
+    subprocess.run(f'7z a -t7z -x"!*.ini" -m0=lzma2:d1024m -mx=9 -mfb=64 -md=32m -ms=on -r "{item}.7z" *.*', shell=True, check=True)
+
+def getParams():
     parser = ArgumentParser("BatchCompress.py")
     parser.add_argument("-type", help="zip or 7z", type=str)
     params = parser.parse_args()
