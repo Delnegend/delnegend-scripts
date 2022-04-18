9aa74c9e787fc69000aad2ebd9c7c9c583acade4
Delnegend <kiennguyen19323@gmail.com>
2022-04-18 10:26:58 +0700

    refactor(batch-compress): add err handler

--- BatchCompress.py
+++ BatchCompress.py
@@ -1,38 +1,37 @@
+from ast import arg
 from asyncio import subprocess
 from os import chdir, listdir
 from os.path import isfile, join
 from shutil import move
-from argparse import ArgumentParser
+from sys import argv
 import subprocess
 
 def main():
-    params = getParams()
-    folders = getFiles(".")
-    for folder in folders:
-        chdir(folder)
-        if params.type == "zip":
-            compress_zip(folder)
-        elif params.type == "7z":
-            compress_7z(folder)
+    folders = getFolders(".")
+    type = argv[1]
+    try:
+        if type == "7z":
+            for folder in folders:
+                chdir(folder)
+                compress_7z(folder)
+        elif type == "zip":
+            for folder in folders:
+                chdir(folder)
+                compress_zip(folder)
+        else:
+            print("Type must be 7z or zip")
         move(f'{folder}.{type}', "..")
         chdir("..")
-
-def getFiles(path):
+    except Exception as e:
+        print("You must specify a type of compression as a first argument: 7z or zip")
+def getFolders(path):
     return [f for f in listdir(path) if not isfile(join(path, f))]
 
 def compress_zip(item):
-    subprocess.run(zip = f'7z.exe a -bt -x"!*.ini" -r "{item}.zip" *.*', shell=True, check=True)
+    subprocess.run(f'7z.exe a -bt -tzip -x"!*.ini" -r "{item}.zip" *.*', shell=True, check=True)
 
 def compress_7z(item):
-    subprocess.run(f'7z a -t7z -x"!*.ini" -m0=lzma2:d1024m -mx=9 -mfb=64 -md=32m -ms=on -r "{item}.7z" *.*', shell=True, check=True)
-
-def getParams():
-    parser = ArgumentParser("BatchCompress.py")
-    parser.add_argument("-type", help="zip or 7z", type=str)
-    params = parser.parse_args()
-    if params.type not in ["zip", "7z"]:
-        raise Exception("-type must be zip or 7z")
-    return params
+    subprocess.run(f'7z.exe a -bt -t7z -x"!*.ini" -m0=lzma2:d1024m -mx=9 -mfb=64 -md=32m -ms=on -r "{item}.7z" *.*', shell=True, check=True)
 
 if __name__ == '__main__':
     try:
