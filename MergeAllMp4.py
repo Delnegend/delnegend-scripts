93900be58ba9f21ebc85bf4c58a594193446c6ee
Delnegend <contact@delnegend.com>
2022-05-22 16:26:26 +0700

    fix(merge-all-mp4): folder name

--- MergeAllMp4.py
+++ MergeAllMp4.py
@@ -2,8 +2,9 @@ import os
 from glob import glob
 from shutil import move
 from re import search
-import subprocess
+import subprocess as sp
 from concurrent.futures import ProcessPoolExecutor, as_completed
+
 
 def list_folders(dir):
     folders = []
@@ -12,6 +13,7 @@ def list_folders(dir):
         if os.path.isdir(path):
             folders.append(path)
     return folders
+
 
 def list_files(dir, recurse=True):
     files = []
@@ -23,22 +25,24 @@ def list_files(dir, recurse=True):
             files.extend(list_files(path, recurse))
     return files
 
-# check if file is corrupt alongside with execution time
+
 def is_corrupt(file):
     ffmpeg_check = f'ffmpeg -y -i "{file}" -t 2 -r 0.5 "{file}.png"'
-    subprocess.call(ffmpeg_check, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    sp.call(ffmpeg_check, shell=True, stdout=sp.DEVNULL, stderr=sp.DEVNULL)
     if os.path.exists(f"{file}.png"):
         os.remove(f"{file}.png")
         return False
     else:
         return True
 
+
 def main():
-    # create a folder for each day and move them into
     all_parts = list_folders(".")
     for item in all_parts:
-        if not search(r"_", item):
-            day_folder = item[slice(6)] + "_" + item[slice(4, 6)] + "_" + item[slice(6, 8)]
+        # if item doesn't contain underscore and regex match \d{10}
+        if not search("_", item) and search("\d{10}", item):
+            day_folder = item[slice(6)] + "_" + \
+                item[slice(6, 8)] + "_" + item[slice(8, 10)]
             if not os.path.exists(day_folder):
                 os.mkdir(day_folder)
             move(item, day_folder)
@@ -46,11 +50,13 @@ def main():
 
     # for each folder in YYYY_MM_DDs, merge every mp4 file into YYYY_MM_DD.mkv
     for day_vid in day_vids:
+        if not search("\d{4}_\d{2}_\d{2}", day_vid):
+            continue
         os.chdir(day_vid)
         all_parts_in_day = list_files(".")
 
         # check for corrupt files and remove them
-        print("Checking for corrupt files...")
+        print(f"Checking for corrupt files of {day_vid}...")
         corrupted_files = []
         with ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
             tasks = {executor.submit(is_corrupt, file): file for file in all_parts_in_day}
@@ -65,8 +71,6 @@ def main():
         for file in corrupted_files:
             print(f"- {file} is corrupted")
         if corrupted_files:
-            print("Enter to remove them and continue, or Ctrl+C to cancel")
-            input()
             for file in corrupted_files:
                 os.remove(file)
 
@@ -87,10 +91,13 @@ def main():
             with open("merge.log", "w") as f:
                 f.write("")
         with open("merge.log", "a") as f:
-            subprocess.call(ffmpeg_merge_all, shell=True, stdout=f, stderr=f)
+            sp.call(ffmpeg_merge_all, shell=True, stdout=f, stderr=f)
         os.remove("merge.txt")
 
         os.chdir("..")
+    print("Press any key to exit...")
+    input()
+
 
 if __name__ == "__main__":
     try:
